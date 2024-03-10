4eab77a8e7fd3a6541b927607ee4155042c243dd
maoty321 <muqtarajiboye>
2024-03-10 23:00:36 +0700

    update file

--- router/routers.js
+++ router/routers.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const router = express.Router()
-const db = require('../db')
+const crudmodel = require('../db')
 
-router.get('/viewData', (req, res)=> {
-    res.send('view Datagit ')
+router.get('/viewData', async(req, res)=> {
+    try {
+        const data = await crudmodel.find()
+        if(data.length > 0) {
+            res.send(data)
+        } else {
+            res.send('no data found')
+        }
+    } catch (error) {
+        
+    }
 })
 
 module.exports = router
