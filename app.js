79c427ac09e5ebe1012f306ee0166f8dc9171cbb
maoty321 <muqtarajiboye>
2024-03-10 22:21:44 +0700

    add app file

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 4000
+const bodyParser  = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.listen(port, ()=> {
+    console.log(`port connected to ${port}`)
+})
